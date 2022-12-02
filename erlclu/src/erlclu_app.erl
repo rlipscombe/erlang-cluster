61bc896f1adad6bd56624c8a8e67363282ffb016
Roger Lipscombe <roger@differentpla.net>
2022-12-02 08:56:59 +0000

    Start SSH daemon

--- erlclu/src/erlclu_app.erl
+++ erlclu/src/erlclu_app.erl
@@ -3,9 +3,18 @@
 -export([start/2, stop/1]).
 
 -include_lib("kernel/include/logger.hrl").
+-define(APPLICATION, erlclu).
 
 start(_StartType, _StartArgs) ->
     ?LOG_INFO("Starting up"),
+
+    SystemDir = filename:join([code:priv_dir(?APPLICATION), "ssh", "system"]),
+    {ok, _} = ssh:daemon(10022, [
+        {system_dir, SystemDir},
+        {pwdfun, fun(_User, _Password, _Peer, _State) -> true end}
+    ]),
+    ?LOG_INFO("SSH listening on port 10022"),
+
     Dispatch = cowboy_router:compile([
         {'_', [
             {"/", home_handler, []},
@@ -17,7 +26,7 @@ start(_StartType, _StartArgs) ->
         [{port, 8080}],
         #{env => #{dispatch => Dispatch}}
     ),
-    ?LOG_INFO("Listening on port 8080"),
+    ?LOG_INFO("Cowboy listening on port 8080"),
     erlclu_sup:start_link().
 
 stop(_State) ->
