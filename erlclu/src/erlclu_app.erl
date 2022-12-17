b69e25fedf76e8c8c205f20284b956b109641bd9
Roger Lipscombe <roger@differentpla.net>
2022-12-17 20:37:53 +0000

    Add prometheus metrics listener

--- erlclu/src/erlclu_app.erl
+++ erlclu/src/erlclu_app.erl
@@ -8,19 +8,9 @@ start(_StartType, _StartArgs) ->
     ?LOG_INFO("Starting up"),
 
     start_ssh_daemon(),
+    start_http_listener(),
+    start_prometheus_listener(),
 
-    Dispatch = cowboy_router:compile([
-        {'_', [
-            {"/", home_handler, []},
-            {"/readyz", readyz_handler, []}
-        ]}
-    ]),
-    {ok, _} = cowboy:start_clear(
-        erlclu_listener,
-        [{port, 8080}],
-        #{env => #{dispatch => Dispatch}}
-    ),
-    ?LOG_INFO("Cowboy listening on port 8080"),
     erlclu_sup:start_link().
 
 start_ssh_daemon() ->
@@ -37,6 +27,35 @@ start_ssh_daemon(SystemDir, UserDir) when is_list(SystemDir), is_list(UserDir) -
     ?LOG_INFO("SSH daemon listening on port 10022");
 start_ssh_daemon(_SystemDir, _UserDir) ->
     ?LOG_WARNING("Not starting SSH daemon").
+
+start_http_listener() ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/", home_handler, []},
+            {"/readyz", readyz_handler, []}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(
+        erlclu_listener,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    ?LOG_INFO("Cowboy listening on port 8080"),
+    ok.
+
+start_prometheus_listener() ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/metrics/[:registry]", erlclu_metrics_handler, []}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(
+        prometheus_listener,
+        [{port, 9153}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    ?LOG_INFO("Metrics on port 9153"),
+    ok.
 
 stop(_State) ->
     ok.
