9c29321773f4cc389f95c3128e5d6b265e4e8d12
Roger Lipscombe <roger@differentpla.net>
2023-01-13 20:46:42 +0000

    DOES NOT WORK
    The CouchDB stuff allows for *this* node to connect to other nodes that
    aren't using TLS. It doesn't allow for this node to *accept* some
    connections without TLS.
    
    To do that, we need to mess around with a custom implementation of
    accept.
    
    OR: there might be a way to run multiple distributions, and have
    firewall rules forward in-pod connections to the non-TLS one.
    
    OR: can we have epmd lie about the port for in-pod queries?
    
    OR: can we (somehow) have nc in a sidecar that adds/removes TLS one way
    or the other? Poor man's service mesh, if you like.

--- erlclu/src/erlclu_dist.erl
+++ erlclu/src/erlclu_dist.erl
@@ -1,1 +1,70 @@
 -module(erlclu_dist).
+-export([
+    childspecs/0,
+    listen/2,
+    accept/1,
+    accept_connection/5,
+    setup/5,
+    close/1,
+    select/1,
+    is_node_name/1
+]).
+
+-include_lib("kernel/include/logger.hrl").
+
+-define(MOD, inet_tls_dist).
+
+childspecs() ->
+    {ok, [
+        {ssl_dist_sup, {ssl_dist_sup, start_link, []}, permanent, infinity, supervisor, [
+            ssl_dist_sup
+        ]}
+    ]}.
+
+listen(Name, Host) ->
+    ?LOG_INFO(#{func => ?FUNCTION_NAME, name => Name, host => Name}),
+    ?MOD:?FUNCTION_NAME(Name, Host).
+
+accept(Listen) ->
+    ?LOG_INFO(#{func => ?FUNCTION_NAME, listen => Listen}),
+    ?MOD:?FUNCTION_NAME(Listen).
+
+accept_connection(AcceptPid, DistCtrl, MyNode, Allowed, SetupTime) ->
+    ?LOG_INFO(#{
+        func => ?FUNCTION_NAME,
+        accept_pid => AcceptPid,
+        dist_ctrl => DistCtrl,
+        my_node => MyNode,
+        allowed => Allowed,
+        setup_time => SetupTime
+    }),
+    ?MOD:?FUNCTION_NAME(AcceptPid, DistCtrl, MyNode, Allowed, SetupTime).
+
+setup(Node, Type, MyNode, LongOrShortNames, SetupTime) ->
+    ?LOG_INFO(#{
+        func => ?FUNCTION_NAME,
+        node => Node,
+        type => Type,
+        my_node => MyNode,
+        long_or_short_names => LongOrShortNames,
+        setup_time => SetupTime
+    }),
+    ?MOD:?FUNCTION_NAME(Node, Type, MyNode, LongOrShortNames, SetupTime).
+
+close(Socket) ->
+    ?LOG_INFO(#{func => ?FUNCTION_NAME, socket => Socket}),
+    ?MOD:?FUNCTION_NAME(Socket).
+
+is_node_name(Node) ->
+    ?LOG_INFO(#{
+        func => ?FUNCTION_NAME,
+        node => Node
+    }),
+    ?MOD:?FUNCTION_NAME(Node).
+
+select(Node) ->
+    ?LOG_INFO(#{
+        func => ?FUNCTION_NAME,
+        node => Node
+    }),
+    ?MOD:?FUNCTION_NAME(Node).
