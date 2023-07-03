982e5d7a09185b2205eb9af2607f358b62f2f71d
Roger Lipscombe <roger@differentpla.net>
2023-07-03 18:02:57 +0100

    Fix '502 Bad Gateway' error

--- erlclu/src/erlclu_app.erl
+++ erlclu/src/erlclu_app.erl
@@ -1,8 +1,13 @@
 -module(erlclu_app).
 -behaviour(application).
--export([start/2, stop/1]).
+-export([
+    start/2,
+    prep_stop/1,
+    stop/1
+]).
 
 -include_lib("kernel/include/logger.hrl").
+-define(APPLICATION, erlclu).
 
 start(_StartType, _StartArgs) ->
     ?LOG_INFO("Starting up"),
@@ -68,5 +73,31 @@ start_prometheus_listener(Port) when is_integer(Port) ->
     ?LOG_INFO("Metrics on port ~B", [Port]),
     ok.
 
+prep_stop(State) ->
+    % Because we're using the same listener for /readyz as everything else, we can't simply stop the listener. If we do
+    % that, then we've literally caused the problem we're trying to avoid.
+
+    % Instead, we have /readyz return an error, so that the readinessProbe takes us out of the ingress/service.
+    % To signal this state, we set an application env var:
+    application:set_env(?APPLICATION, erlclu_listener_ready, false),
+
+    % We need to continue responding until we're removed from the ingress. Because the probe is configured for every
+    % 10 seconds (see the deployment yaml), we need to wait for around 20s before stopping.
+
+    % K8s gives us 30s between SIGTERM and SIGKILL, so 20s is fine.
+
+    % If we were dealing with relatively long-lived connections (we're not), we would also wait until they'd drained.
+    % Because we're sharing the listener, this would need to be done in several steps:
+    % 1. disable /readyz
+    % 2. wait until the probe fails
+    % 3. stop the listener
+    % 4. wait until existing connections are drained
+    % 5. stop
+    ShutdownDelayMs = list_to_integer(os:getenv("SHUTDOWN_DELAY_MS", "20000")),
+    ?LOG_INFO("Delaying shutdown for ~B ms", [ShutdownDelayMs]),
+    timer:sleep(ShutdownDelayMs),
+    State.
+
 stop(_State) ->
+    ?LOG_INFO("Stopped"),
     ok.
